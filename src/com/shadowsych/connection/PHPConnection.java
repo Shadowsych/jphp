e008f6f766c9c87d7553a9cd6875ad33a1d01077
Pravat Bhusal <pravat.bhusal@gmail.com>
2017-10-29 10:46:57 -0500

    Reformat the PHPConnection.java syntax

--- src/com/shadowsych/connection/PHPConnection.java
+++ src/com/shadowsych/connection/PHPConnection.java
@@ -6,34 +6,34 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.Map;
 
 public class PHPConnection {
 	public PHPConnection() {
 	}
-	public String HTTPRequest(String urlRequest, String requestMethod, Map<String,String> requestVariables) {
-		if(requestMethod.equalsIgnoreCase("GET")) {
+
+	public String HTTPRequest(String urlRequest, String requestMethod, Map<String, String> requestVariables) {
+		if (requestMethod.equalsIgnoreCase("GET")) {
 			try {
-				//re-format and append variables to the GET URL
+				// re-format and append variables to the GET URL
 				urlRequest += "?";
 				Object[] getVariables = requestVariables.entrySet().toArray();
-				for(int i = 0; i < requestVariables.size(); i++) {
+				for (int i = 0; i < requestVariables.size(); i++) {
 					Object getVariable = getVariables[i];
 					getVariable = getVariable.toString().replaceAll(" ", "%20");
 					urlRequest += getVariable + "&";
 				}
-				
-				//establish the new connection
+
+				// establish the new connection
 				URL url = new URL(urlRequest);
 				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 				conn.setRequestMethod("GET");
-				
-				//read the results and return the output
+
+				// read the results and return the output
 				BufferedReader outputReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 				StringBuffer outputString = new StringBuffer();
 				String line;
-				while((line = outputReader.readLine()) != null) {
+				while ((line = outputReader.readLine()) != null) {
 					outputString.append(line);
 				}
 				outputReader.close();
@@ -41,50 +41,47 @@ public class PHPConnection {
 			} catch (IOException e) {
 				return e.toString();
 			}
-		} else if(requestMethod.equalsIgnoreCase("POST")) {
+		} else if (requestMethod.equalsIgnoreCase("POST")) {
 			try {
-				 //re-format the data for POST variables
-				 urlRequest += "?";
-				 String data = "";
-				 Object[] getVariables = requestVariables.entrySet().toArray();
-				 for(int i = 0; i < requestVariables.size(); i++) {
+				// re-format the data for POST variables
+				String data = "";
+				Object[] getVariables = requestVariables.entrySet().toArray();
+				for (int i = 0; i < requestVariables.size(); i++) {
 					Object getVariable = getVariables[i];
 					getVariable = getVariable.toString().replaceAll(" ", "%20");
 					data += getVariable + "&";
-			     }
-				 
-				 //establish the new connection
-			     URL url = new URL(urlRequest);
-			     HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-				 
-			     //send data via a POST request
-			     conn.setDoOutput(true);
-			     conn.setInstanceFollowRedirects(true);
-			     conn.setRequestMethod("POST");
-			     conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			     conn.setRequestProperty("charset", "UTF-8");
-			     conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
-			     conn.setUseCaches(false);
-			     try(OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream()))
-			     {
-			          out.write(data);
-			     } 
+				}
 
-			     //read the results and return the output
-			     BufferedReader outputReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			     StringBuffer outputString = new StringBuffer();
-			     String line;
-			     while((line = outputReader.readLine()) != null)
-			     {
-			    	 outputString.append(line);
-			     }
-			     outputReader.close();
-			     return outputString.toString().replaceAll("<br >", "\n");	
-			}  catch (IOException e) {
+				// establish the new connection
+				URL url = new URL(urlRequest);
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+				// send data via a POST request
+				conn.setDoOutput(true);
+				conn.setInstanceFollowRedirects(true);
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+				conn.setRequestProperty("charset", "UTF-8");
+				conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+				conn.setUseCaches(false);
+				try (OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream())) {
+					out.write(data);
+				}
+
+				// read the results and return the output
+				BufferedReader outputReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				StringBuffer outputString = new StringBuffer();
+				String line;
+				while ((line = outputReader.readLine()) != null) {
+					outputString.append(line);
+				}
+				outputReader.close();
+				return outputString.toString();
+			} catch (IOException e) {
 				return e.toString();
 			}
 		} else {
-			return "shadowsych.httprequest Exception: Please specify your HTTP request as either POST or GET.";
+			return "shadowsych.connection Exception: Please specify your HTTP request as either POST or GET.";
 		}
 	}
 }
